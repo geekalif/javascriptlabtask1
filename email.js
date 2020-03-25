c578a6d001f954931cefa27414200eadf6bcfe76
geekalif <60336299+geekalif@users.noreply.github.com>
2020-03-25 16:06:53 +0600

    Third commit

--- email.js
+++ email.js
@@ -3,8 +3,12 @@ function checkEmpty(email) {
     return !email.trim().length;
 }
 
+function emailValidation(email){
+  return /\S+@\S+\.\S+/.test(email);
+}
+
 function executeLogic(getElementById('submit').onclick("Click"), function(){
-  if(checkEmpty(this.value)) {
+  if(checkEmpty(this.value) && emailValidation(this.value)) {
     console.log( "email is valid" );
   } else {
     console.log( "email is not valid" );
---
+++ gender.js
@@ -0,0 +1,21 @@
+
+function checkGenderRadioButton(){
+  var radioOption = document.getElementsByName("gender");
+   var formValid = false;
+
+   var i = 0;
+
+   while (!formValid && i < radios.length) {
+           if (radios[i].checked) formValid = true;
+           i++;
+       }
+    return formValid;
+}
+
+function executeLogic(getElementById('submit').onclick("Click"), function(){
+  if(checkGenderRadioButton(this.value)) {
+    console.log( "Gender is valid" );
+  } else {
+    console.log( "Gender is not valid" );
+  }
+});
